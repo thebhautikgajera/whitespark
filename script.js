ff47dd32e916448cf329369b0a676a24dfe282de
bhautikpatel13 <bhautikpatel464@gmail.com>
2024-07-15 12:23:13 +0530

    4th commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
