e024f215d44e52d1235ff8d98ff7d1887cce91b7
bhautikpatel13 <bhautikpatel464@gmail.com>
2024-07-25 22:20:27 +0530

    18th commit

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-//Responsive
-
-tailwind.config = {
-  theme: {
-    screens: {
-      sm: { min: "320px", max: "480px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
-
-      md: { min: "481px", max: "1024px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-
-      lg: { min: "1025px" },
-    },
-  },
-};
-
-//Slider-1
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -47,22 +25,113 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-// accordion
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot-mobile");
+//   if (n > slides.length) { slideIndex = 1 }
+//   if (n < 1) { slideIndex = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot-tablet");
+//   if (n > slides.length) { slideIndex = 1 }
+//   if (n < 1) { slideIndex = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
+
+// accordion-page-12
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active2");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    var panell = this.nextElementSibling;
+    if (panell.style.display === "block") {
+      panell.style.display = "none";
     } else {
-      panel.style.display = "block";
+      panell.style.display = "block";
     }
   });
 }
 
+//accordation in laptop
+var ac = document.getElementsByClassName("accordion1");
+var j;
+
+for (j = 0; j < ac.length; j++) {
+  ac[j].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var pane = this.nextElementSibling;
+    if (pane.style.display === "block") {
+      pane.style.display = "none";
+    } else {
+      pane.style.display = "block";
+    }
+  });
+}
+
+var ac = document.getElementsByClassName("accordion1");
+var j;
+
+for (j = 0; j < ac.length; j++) {
+  ac[j].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var pane = this.nextElementSibling;
+    if (pane.style.maxHeight) {
+      pane.style.maxHeight = null;
+    } else {
+      pane.style.maxHeight = pane.scrollHeight + "px";
+    }
+  });
+}
+
+
+
+//navbar accordation in tablet and mobile
+var acc = document.getElementsByClassName("dropdownNav");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("activeNav");
+
+    /* Toggle between hiding and showing the active panel */
+    var panelNav = this.nextElementSibling;
+    if (panelNav.style.display === "block") {
+      panelNav.style.display = "none";
+    } else {
+      panelNav.style.display = "block";
+    }
+  });
+}
+
+
+//Slider-2-Desktop
 //Slider-2
 // DOM utility functions:
 
@@ -216,3 +285,31 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+
+//Swiper js - mobile & tablat
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
